4627ddc28110da61e848f28eebb78ddda0f00f0e
nagoo <nagoo@sharif-monitor.lan>
2025-09-26 10:19:59 -0500

    commit

---
+++ Week2_programs/src/Week2/Program1_PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package Week2;
+
+public class Program1_PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 30000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 99.9999999;
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + c);
+        System.out.println("Boolean value: " + bool);
+    }
+}

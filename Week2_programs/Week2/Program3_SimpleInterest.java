b5cd16414f75c020ecb6228a29800072b42d85a0
nagoo <nagoo@sharif-monitor.lan>
2025-10-03 12:14:02 -0500

    commit

--- Week2_programs/Week2/Program3_SimpleInterest.java
+++ Week2_programs/Week2/Program3_SimpleInterest.java
@@ -1,5 +1,25 @@
 package Week2;
 
-public class Program3_SimpleInterest {
+import java.util.Scanner;
 
+public class Program3_SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal (P): ");
+        double P = sc.nextDouble();
+
+        System.out.print("Enter Time (T in years): ");
+        double T = sc.nextDouble();
+
+        System.out.print("Enter Rate (R in %): ");
+        double R = sc.nextDouble();
+
+        double SI = (P * T * R) / 100;
+        System.out.println("Simple Interest = " + SI);
+
+        sc.close();
+    }
 }
+
+//tested
--- Week2_programs/Week2/Program4_SwapNumbers.java
+++ Week2_programs/Week2/Program4_SwapNumbers.java
@@ -1,5 +1,28 @@
 package Week2;
 
-public class Program4_SwapNumbers {
+import java.util.Scanner;
 
+public class Program4_SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+        // Swapping without third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+
+        sc.close();
+    }
 }
+
+//Tested
